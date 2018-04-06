abee53f13874fac26db1ac53af3342ecc95f13dc
Berry  Semexan <bsemexan@gmail.com>
2018-04-06 05:56:03 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search
-    self.all.count
+  def self.search
+    self.all
   end
 end
